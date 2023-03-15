335f48e68c99e596f50dded3134f6d831a293411
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2023-03-15 11:11:48 +0900

    Changes INSTALL Makefile.PL README ggdrv.pm などモジュールを作る為に最小限必要なファイルを揃えた。

---
+++ ggdrv.pm
@@ -0,0 +1,32 @@
+package App::ggdrv ;  
+our $VERSION = '0.03' ; 
+our $DATE = '2023-03-15T09:09+09:00' ; 
+
+=encoding utf8
+
+=head1 NAME
+
+App::ggdrv
+
+=head1 SYNOPSIS
+
+This module provides a Unix-like command `F<ggdrv>'. 
+
+=head1 DESCRIPTION
+
+=head1
+
+ 
+=head1 AUTHOR
+
+下野寿之 Toshiyuki SHIMONO <bin4tsv@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2023 Toshiyuki SHIMONO. All rights reserved.
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
+
+1 ;
