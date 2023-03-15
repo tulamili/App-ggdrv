e1fff46a2b1c066834b6e5992539ad095444db6b
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2023-03-15 23:39:25 +0900

    sync5.pm : テストしたら、updateの引数の順番が間違っていたので、改訂。

--- lib/App/ggdrv/sync5.pm
+++ lib/App/ggdrv/sync5.pm
@@ -11,22 +11,18 @@ sub cat ( $ ) { return qx[ cat $_[0] ] } # ファイルの中身を取り出す�
 sub sync5 {
   $ARGV[1] //= '.' ;
   my $dir0 = cwd ;
-  my $dir1 = shift @ARGV ; # qx [ mkdir -p $dir ] if $o{p} ; 
-  my $dir2 = shift @ARGV ; # qx [ mkdir -p $dir ] if $o{p} ; 
-   my @cmd ; 
+  my $dir1 = shift @ARGV ; # OLD ; qx [ mkdir -p $dir ] if $o{p} ; 
+  my $dir2 = shift @ARGV ; # NEW ; qx [ mkdir -p $dir ] if $o{p} ; 
+  my @cmd ; 
   while ( <> ) {
     chomp ; 
     my @F = split /\t/ , $_ ;
     next if ! -f "$dir2/$F[2]" ; # 新しいファイルがそもそも存在しないなら、何もしない。
-    #push @cmd , qq [$FindBin::Bin/update12.pl $F[1]:$F[2] ] if ! -f "$dir1/$F[2]" || (cat "$dir1/$F[2]") ne (cat "$dir2/$F[2]") ; 
-    push @cmd , qq [$0 update -20 $F[1] $F[2] ] if ! -f "$dir1/$F[2]" || (cat "$dir1/$F[2]") ne (cat "$dir2/$F[2]") ; 
+    push @cmd , qx { $0 update -20 $dir2/$F[2] $F[1] } if ! -f "$dir1/$F[2]" || (cat "$dir1/$F[2]") ne (cat "$dir2/$F[2]") ; 
   }
-  chdir $dir2 or die ;
   for ( @cmd ){ 
-    say $_ ;
-    say " --> " , qx [ $_ ]  =~ s/\n\s+/ /grs =~ s/\n/ /grs ; 
+    say " --> " , qq [ $_ ]  =~ s/\n\s+/ /grs =~ s/\n/ /grs ; 
   }
-  chdir $dir0 or die ; 
 }
   
 =encoding utf8
@@ -44,12 +40,10 @@ sub sync5 {
     つまり、OLD_DIR は、そのGoogleドライブと毎回あらかじめ、きちんと同期されていると仮定してある。)
    VAR_FILEの各行が表す各ファイルfileについて、OLD_DIR/file が NEW_DIR/file と内容が違っていたら、
    グーグルドライブにアップロード。ここで、OLD_DIRは グーグルドライブと download5により同期されていると想定。
-
     約1.5秒ごとに1ファイルを更新するであろう。
 
- オプション : 
  
  開発メモ : 
-   * cwd を用いた。getcwd ではなく。
+   * 動作の様子の表示が、あまり親切では無いかもしれない。改善したい。
    
 =cut
