4cd32f77fa59ecfc68bb2df2244987988cf2b851
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2023-03-16 11:41:55 +0900

    ggdrv.pm : 文面未更新を修正。

--- ggdrv.pm
+++ ggdrv.pm
@@ -1,6 +1,6 @@
 package App::ggdrv ;  
-our $VERSION = '0.030' ; 
-our $DATE = '2023-03-16T09:09+09:00' ; 
+our $VERSION = '0.031' ; 
+our $DATE = '2023-03-16T11:45+09:00' ; 
 
 =encoding utf8
 
@@ -15,13 +15,14 @@ This module App::ggdrv provides a Unix-like command `F<ggdrv>'.
 =head1 DESCRIPTION
 
 =head1
-
  ggdrv 
 
   サブコマンド方式で、様々な関連する機能を実行可能としている。
   OAuthの仕組みでGoogleドライブにファイルを自動更新で同期する機能を提供する。
 
  利用例 :
+
+   ggdrv --help  # --help により、このヘルプの文面が現れる。
 
   [設定系]
    ggdrv   # 引数無しで実行すると、最初のセットアップの仕方(ブラウザ越しで「クライアントシークレット」を作る方法)が表示される。
@@ -30,7 +31,7 @@ This module App::ggdrv provides a Unix-like command `F<ggdrv>'.
    ggdrv tokens  #  トークンの情報2個を単に表示する。(リフレッシュトークン(使わないと半年で無効化)とアクセストークンがある。)
    ggdrv tokens --get   # トークンを取得する(設定ファイルに書き込む)。書き込む前のファイルはバックアップは1回のみ保管。
    ggdrv tokens --get --try   # トークンを取得するが、設定ファイルには書き込まない。
-   ggdrv atoken  # アクセストークンのみ更新する。60分未使用であれば、更新は必要。-try を指定すると設定ファイルに書き込まない。
+   ggdrv tokens --atoken  # アクセストークンのみ更新する。60分未使用であれば、更新は必要。-try を指定すると設定ファイルに書き込まない。
 
   [ファイル検索]
    ggdrv fsearch FILENAME # ファイルを最大100個まで探す(フォルダは指定不能)。ワイルドカードを指定可能。タブ区切り5列で出力。
@@ -52,8 +53,6 @@ This module App::ggdrv provides a Unix-like command `F<ggdrv>'.
  環境変数 : export VAR=VALUE で指定。unset VAR で解除が可能。
 
    GGDRV_API    :  クライアントIDやクライアントシークレットを保管するファイルの名前。未指定なら ~/ggdrv2303v1 である。
-
-
  
 =head1 AUTHOR
 
